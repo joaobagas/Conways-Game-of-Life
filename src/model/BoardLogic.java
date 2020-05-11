12d040451cd3ec1ecec8e9eed17ff0bd8762b449
joaobagas <joaobagas@users.noreply.github.com>
2020-05-11 19:31:52 +0200

    Created some logic for the "board"

---
+++ src/model/BoardLogic.java
@@ -0,0 +1,52 @@
+package model;
+
+public class BoardLogic {
+    public boolean[][] resetBoard() {
+        boolean[][] board;
+        board = new boolean[][]{{false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
+                                {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
+                                {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
+                                {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
+                                {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
+                                {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
+                                {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
+                                {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
+                                {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
+                                {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
+                                {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
+                                {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
+                                {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
+                                {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
+                                {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false}};
+        return board;
+    }
+
+    public boolean[][] newGeneration(int numberOfCells, boolean[][] board) {
+        boolean[][] tempBoard = resetBoard();
+        for (int i = 0; i < numberOfCells; i++) { for (int j = 0; j < numberOfCells; j++) { tempBoard[j][i] = isAlive(i, j, numberOfCells, board); } }
+        return tempBoard;
+    }
+
+    public boolean isAlive (int x, int y, int numberOfCells, boolean[][] board) {
+        if (board[y][x]) {
+            if      (checkNumberOfNeighbors(x, y, numberOfCells, board) < 2) { return false; }
+            else if (checkNumberOfNeighbors(x, y, numberOfCells, board) > 3) { return false; }
+            else                                      { return true;  }
+        }
+        else { if (checkNumberOfNeighbors(x, y, numberOfCells, board) == 3)  {  return true; } }
+        return false;
+    }
+
+    public int checkNumberOfNeighbors(int x, int y, int numberOfCells, boolean[][] board) {
+        int count = 0;
+        if (y > 0 && x > 0)                             { if (board[y-1][x-1]) { count++; } }
+        if (y > 0)                                      { if (board[y-1][x])   { count++; } }
+        if (x > 0)                                      { if (board[y][x-1])   { count++; } }
+        if (y < numberOfCells-1 && x < numberOfCells-1) { if (board[y+1][x+1]) { count++; } }
+        if (y < numberOfCells-1)                        { if (board[y+1][x])   { count++; } }
+        if (x < numberOfCells-1)                        { if (board[y][x+1])   { count++; } }
+        if (y < numberOfCells-1 && x > 0)               { if (board[y+1][x-1]) { count++; } }
+        if (y > 0 && x < numberOfCells-1)               { if (board[y-1][x+1]) { count++; } }
+        return count;
+    }
+}
--- src/model/GameOfLifeLogic.java
+++ src/model/GameOfLifeLogic.java
@@ -1,4 +1,16 @@
 package model;
 
 public class GameOfLifeLogic {
+    private BoardLogic boardLogic;
+    private boolean[][] board;
+    private int numberOfCells;
+
+    public GameOfLifeLogic() {
+        resetBoard();
+        numberOfCells = 15; // Because the grid will be 15*15
+    }
+
+    public void resetBoard() { board = boardLogic.resetBoard(); }
+
+    public void newGeneration() { board = boardLogic.newGeneration(numberOfCells, board); }
 }
